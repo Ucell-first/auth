1e3d0657c419bda64f476348dbb8cc20b71d8446
nurmuhammad701 <nurmuhammadmel@gmail.com>
2025-06-18 16:14:00 +0500

    middleware is added

---
+++ api/middleware/middleware.go
@@ -0,0 +1,93 @@
+package middleware
+
+import (
+	"auth/tokens"
+	"errors"
+	"net/http"
+
+	"github.com/casbin/casbin/v2"
+	"github.com/gin-gonic/gin"
+)
+
+type CasbinPermission interface {
+	GetRole(*gin.Context) (string, int)
+	CheckPermission(*gin.Context) (bool, error)
+	CheckPermissionMiddleware() gin.HandlerFunc
+}
+
+type casbinPermission struct {
+	enforcer *casbin.Enforcer
+}
+
+func NewCasbinPermission(enforcer *casbin.Enforcer) CasbinPermission {
+	return &casbinPermission{enforcer: enforcer}
+}
+
+func Check(c *gin.Context) {
+	acccesorrefresh := c.GetHeader("Authorization")
+	if acccesorrefresh == "" {
+		c.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{
+			"error": "Authorization is required",
+		})
+		return
+	}
+
+	_, err := tokens.ValidateACCESToken(acccesorrefresh)
+	if err != nil {
+		c.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{
+			"error": "Invalid token provided",
+		})
+		return
+	}
+	c.Next()
+}
+
+func (casb *casbinPermission) GetRole(c *gin.Context) (string, int) {
+	token := c.GetHeader("Authorization")
+	if token == "" {
+		return "unauthorized", http.StatusUnauthorized
+	}
+	_, role, err := tokens.GetUserInfoFromACCESToken(token)
+	if err != nil {
+		return "error while reding role", 500
+	}
+
+	return role, 0
+}
+
+func (casb *casbinPermission) CheckPermission(c *gin.Context) (bool, error) {
+
+	act := c.Request.Method
+	sub, status := casb.GetRole(c)
+	if status != 0 {
+		return false, errors.New("error in get role")
+	}
+	obj := c.FullPath()
+
+	ok, err := casb.enforcer.Enforce(sub, obj, act)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"Error": "Internal server error",
+		})
+		c.Abort()
+		return false, err
+	}
+	return ok, nil
+}
+
+func (casb *casbinPermission) CheckPermissionMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		result, err := casb.CheckPermission(c)
+
+		if err != nil {
+			c.AbortWithError(500, err)
+		}
+		if !result {
+			c.AbortWithStatusJSON(401, gin.H{
+				"message": "Forbidden",
+			})
+		}
+
+		c.Next()
+	}
+}
